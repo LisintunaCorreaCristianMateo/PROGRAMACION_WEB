5db9db99dab229e39edc9e3e1cff9df25d7a5bfe
LisintunaCorreaCristianMateo <cmlisintuna@espe.edu.ec>
2025-05-11 17:28:32 -0500

    Cambios subidos

--- TR_TEMPLATE/assets/demo/chart-area-demo.js
+++ TR_TEMPLATE/assets/demo/chart-area-demo.js
@@ -1,54 +1,34 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#292b2c';
-
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
-    },
-    legend: {
-      display: false
+function initAreaChart() {
+    const ctxArea = document.getElementById('myAreaChart');
+    if (ctxArea) {
+        new Chart(ctxArea, {
+            type: 'line',
+            data: {
+                labels: ['2017','2018', '2019', '2020', '2021', '2022','2023'],
+                datasets: [{
+                    label: 'PIB Ecuador',
+                    data: [5.3,1.2,-1.5,-11.8,11.2,7.7,3.6],
+                    fill: true,
+                    borderColor: 'rgb(23, 20, 18)',
+                    backgroundColor: 'rgba(240, 139, 44, 0.2)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'PIB Ecuador (Millones USD)'
+                    }
+                }
+            }
+        });
     }
-  }
-});
+}
+
+// Exportar la función para usarla en cargador_paginas.js
+window.initAreaChart = initAreaChart;
